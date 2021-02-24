4d8e041eede5987b5ad3bee8c16db84368dd2749
ACricketToFillTheSilence <richellecohen526@gmail.com>
2021-02-23 19:44:40 -0800

    Removed default text from Home
    Removed default text from the home screen UI.

--- app/src/main/java/com/e/halfbaked/ui/home/HomeViewModel.java
+++ app/src/main/java/com/e/halfbaked/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        //mText.setValue("This is home fragment"); Writes a text fragment on screen
     }
 
     public LiveData<String> getText() {
